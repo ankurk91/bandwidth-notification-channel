ed65439d8ec198abde7c3f1f2a4acede0f8be8d9
ankurk91 <pro.ankurk1@gmail.com>
2018-08-20 19:11:32 +0530

    Install phpunit and mockery package

--- tests/ChannelTest.php
+++ tests/ChannelTest.php
@@ -2,8 +2,12 @@
 
 namespace NotificationChannels\Bandwidth\Test;
 
+use Exception;
+use Mockery;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Bandwidth\BandwidthChannel;
 
 class ChannelTest extends \PHPUnit_Framework_TestCase
 {
-    //todo
+    // todo
 }
--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -2,6 +2,7 @@
 
 namespace NotificationChannels\Bandwidth\Test;
 
+use NotificationChannels\Bandwidth\BandwidthMessage;
 
 class MessageTest extends \PHPUnit_Framework_TestCase
 {
