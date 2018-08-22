937595cb5a8b44c19c06de17a406905342a0da6c
ankurk91 <pro.ankurk1@gmail.com>
2018-08-22 18:10:23 +0530

    Test case for BandwidthConfig class

---
+++ tests/ConfigTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace NotificationChannels\Bandwidth\Test;
+
+use NotificationChannels\Bandwidth\BandwidthConfig;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @var BandwidthConfig
+     */
+    protected $config;
+
+    /**
+     * @var array
+     */
+    protected $params = [
+        'user_id' => 'user_123',
+        'api_token' => 'token_123',
+        'api_secret' => 'secret_123',
+        'from' => '+123456789',
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->config = new BandwidthConfig($this->params);
+    }
+
+    /** @test */
+    public function it_returns_the_user_id()
+    {
+        $this->assertEquals($this->params['user_id'], $this->config->getUserId());
+    }
+
+    /** @test */
+    public function it_returns_the_api_token()
+    {
+        $this->assertEquals($this->params['api_token'], $this->config->getApiToken());
+    }
+
+    /** @test */
+    public function it_returns_the_api_secret()
+    {
+        $this->assertEquals($this->params['api_secret'], $this->config->getApiSecret());
+    }
+
+    /** @test */
+    public function it_returns_the_http_debug_default_value_false()
+    {
+        $this->assertEquals(false, $this->config->shouldDebug());
+    }
+
+    /** @test */
+    public function it_returns_the_http_debug_default_value_when_set()
+    {
+        $config = new BandwidthConfig([
+            'debug_http' => true
+        ]);
+        $this->assertEquals(true, $config->shouldDebug());
+    }
+
+    /** @test */
+    public function it_returns_the_from_value_when_set()
+    {
+        $this->assertEquals($this->params['from'], $this->config->getFrom());
+    }
+
+    /** @test */
+    public function it_returns_the_from_value_as_null_when_not_set()
+    {
+        $config = new BandwidthConfig([]);
+        $this->assertEquals(null, $config->getFrom());
+    }
+
+}
